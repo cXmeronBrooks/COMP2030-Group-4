196c9f0f230632f9dd59439fd016efbb6911d4ec
lich472 <nguyenthanhlich18052003@gmail.com>
2024-10-17 12:14:02 +1030

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function ShowPassword(){
         pass.type = "password";
     }
 }
+
+function setMenuSelected()
+{
+    const menu = document.querySelectorAll("#menu a");
+    for (let opt of menu) {
+        if (document.location.href.includes(opt.href)) {
+            opt.classList.add("selected");
+            break;
+        }
+    }
+}
+
+setMenuSelected();
+
+// Function to show an alert if the status is successful
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const status = urlParams.get('status');
+
+    if (status === 'success') {
+        alert('updated successfully!');
+    }
+};
+
