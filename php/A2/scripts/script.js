93882a4e7d6189443fd993d8ebb21f2b7c7c77e7
lich472 <nguyenthanhlich18052003@gmail.com>
2024-10-20 22:42:14 +1030

    Delete php directory

--- php/A2/scripts/script.js
+++
@@ -1,9 +0,0 @@
-// Function to show an alert if the status is successful
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const status = urlParams.get('status');
-
-    if (status === 'success') {
-        alert('updated successfully!');
-    }
-};
